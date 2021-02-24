24c33d2f40369574e14c81d7d5f46c02a8c3afcc
lperdue <lperdue@cs.odu.edu>
2021-02-23 21:19:38 -0500

    Initial commit

---
+++ src/test/java/KeyWordTest.java
@@ -0,0 +1,17 @@
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.store.RAMDirectory;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class KeyWordTest {
+
+    @org.junit.Test
+    public void searchIndex() {
+
+
+
+    }
+}
